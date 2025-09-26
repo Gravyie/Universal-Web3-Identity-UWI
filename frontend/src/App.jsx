bb6100605973cf392a1da69922d4d2cdbd6e7a31
Gravyie <gappisingh08@gmail.com>
2025-09-26 22:25:08 +0530

    configured tailwind

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+
 function App() {
 
   return (
-    <>
-      
-    </>
+    <div className='text-red-500'>
+      Welcome to UWI
+    </div>
   )
 }
 
