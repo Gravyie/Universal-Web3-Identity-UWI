d5bc42b0ff1a79e5f3654185be3b989c763010a6
Gravyie <gappisingh08@gmail.com>
2025-09-28 06:23:45 +0530

    misc.

--- frontend/src/components/IdentityDashboard.jsx
+++ frontend/src/components/IdentityDashboard.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function IdentityDashboard({ userData }) {
   return (
     <div className="font-mono text-green-400 p-6 bg-black rounded-lg border border-green-600 whitespace-pre-wrap select-none">
-      <div className="mx-auto text-3xl font-bold mb-6">UWI ID</div>
+      <div className="text-3xl font-bold mb-6">UWI ID</div>
       <pre>{JSON.stringify(userData, null, 2)}</pre>
     </div>
   )
